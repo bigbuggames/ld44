2406db50bec1fc1ae88ab85f5a58b92b5a487823
Marc Solà Nadal <marcsolanadal@gmail.com>
2019-04-28 21:12:16 +0200

    Add business values constants

---
+++ src/constants/businessValues.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 0
+    title: "Free shipping",
+    description: "Everywhere, on every item, and for everyone, no need for loyalty programs.",
+    image: "images/free_shipping.png"
+  },
+  {
+    id: 1,
+    title: "Trust",
+    description: "Every Item comes with an authenticity certificate signed by the devil himself.",
+    image: "images/signature.png"
+  },
+  {
+    id: 2,
+    title: "Customer Centric",
+    description: "Here in LLT we value our customers above all else, as long as they don't complain.",
+    image: "images/customer_centric.png"
+  },
+  {
+    id: 3,
+    title: "Value",
+    description: "Spend your time safely knowing that you are getting the best quality for the price paid.",
+    image: "images/value.png"
+  }
+]
