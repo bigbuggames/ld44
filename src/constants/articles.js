d62d34db6f1d41c615d0c12976fe52e7370337fd
Marc Solà Nadal <marcsolanadal@gmail.com>
2019-05-02 21:28:46 +0200

    Add new prices for articles

--- src/constants/articles.js
+++ src/constants/articles.js
@@ -3,11 +3,11 @@ export default {
     8: {
       id: 8,  
       name: 'Ring of Arouse',
-      description: "Doesn't matter if you are looking to spice things up with your couple or compensating for the secondary effects on Furtiva Lagrima. Ring of Arouse ensures an everlasting life of wildness with your loved one! No more headache, no more \"I am tired\", no excuses just passion! Just remember, once the ring is put on, it can't be removed. Approved by Beyoncé.",
+      description: "Doesn't matter if you are looking to spice things up with your couple or compensating for the secondary effects on Furtiva Lagrima. Ring of Arouse ensures an everlasting life of wildness with your loved one! Just remember, once the ring is put on, it can't be removed. Approved by Beyoncé.",
       image: 'images/ring_of_arousal.png',
       price: {
-        percentage: 5,
-        years: 2
+        percentage: 6,
+        years: 3
       },
       outOfOrder: true
     },
@@ -18,8 +18,8 @@ export default {
       category: 'misc',
       image: 'images/collar_dog.png',
       price: {
-        percentage: 5,
-        years: 2
+        percentage: 3,
+        years: 3
       },
       fate: {
         solo: "After your death no one will ever remove your dog's collar, so it will roam the world looking for you until the sun engulfs Earth, damaging the collar enough to fall.",
@@ -33,8 +33,8 @@ export default {
       category: 'misc',
       image: 'images/collar_cat.png',
       price: {
-        percentage: 5,
-        years: 2
+        percentage: 3,
+        years: 3
       },
       fate: {
         solo: "After your death no one will ever remove your cat's collar, so it will roam the world sleeping on trees and not giving a fuck about anything until the sun engulfs Earth, damaging the collar enough to fall.",
@@ -48,8 +48,8 @@ export default {
       category: 'health',
       image: 'images/regrowth_tonic.png',
       price: {
-        percentage: 5,
-        years: 2
+        percentage: 2,
+        years: 4
       },
       fate: {
         solo: "Thanks to that Regrowth Tonic you could finally dust off the comb you didn't need since your early 20's. Trouble began when not only your hair grew back but fur started to cover your whole body. Friends and family stopped recognizing you. Then you realized that shaving only made you become an oversized velcro strap and laser hair removal was useless. Finally you turned to your last option, attending Star Wars Convention after Star Wars convention where you could at least be yourself. Wookie's life until the end of your days isn't the best choice but well, It could be worse.",
@@ -63,8 +63,8 @@ export default {
       description: "\"Dieticians hate it\". Of course they do, after eating that apple you won't need to eat healthy ever again! Actually you could survive on eating wood, dust, glass.... you name it! The effects are permanent, and why wouldn't they? There only downside it is that you'll always be shitting spicy, minor stuff.",
       image: 'images/dietapple.png',
       price: {
-        percentage: 5,
-        years: 2
+        percentage: 2,
+        years: 7
       },
       fate: {
         solo: "Dietapple allowed you to devour the greatest pizzas, the fattest burritos and unhealthy amounts of chocolate, the problem was the other part. And why wouldn't? Shitting became a nightmare, fear took control of your body as soon as you felt nature's call. It wasn't spicy poop, it was fire, lava, true hell. You died after a midnight dump started a fire that burned the whole neighborhood.",
@@ -79,7 +79,7 @@ export default {
       image: 'images/furtiva_lagrima.png',
       price: {
         percentage: 5,
-        years: 2
+        years: 6
       },
       fate: {
         solo: "Since the day you spilled that tiny flask on your loved one, she never left your side. People never understood why would someone be attracted to you but they never asked, at least it seemed something intellectual as she keeps her distance most of the time. A few years later that distance made you break the relationship, which finally lead her to kill herself. After some years being chased by guilt you joined her in a shakespearean fashion.",
@@ -93,8 +93,8 @@ export default {
       description: "Have trouble meeting baes? Always felt like Awsin Powers shouldn't have got his mojo back because you are the one who should have it? Do you want to be the synonym for sex, drugs and Rock n' Roll? well stop asking questions, look no further!",
       image: 'images/powers_mojo.png',
       price: {
-        percentage: 5,
-        years: 2
+        percentage: 12,
+        years: 7
       },
       fate: {
         solo: "Your greediness for sex, attention and ego led you to purchase Power's Mojo. It proved to be a bad choice since the moment you injected that shit, you became irrationally attractive for both male and female, animal and human, the nightmare stretched day and night. The line was crossed when your parents offered you a ménage à trois during a family dinner... You ended up leaving everything and everyone to go live on a desert island. The irony of the situation was finally what killed you, yes, death by irony.",
@@ -108,8 +108,8 @@ export default {
       description: "Looks like a regular mug, but the last half is always full of hot freshly made coffee, doesn't seem like a good idea? Think again. Disclaimer: If staying awake is what you're after we sell that too, look for Owl Droplets. This cup is for coffee lovers only!",
       image: 'images/coffee_mug.png',
       price: {
-        percentage: 5,
-        years: 2
+        percentage: 2,
+        years: 4
       },
       fate: {
         solo: "One day your life took a turn for the best, you acquired a magical coffee mug that simply made everyday a better day. Somehow that had a positive effect on your attitude that made life much much easier. And you probably got married, had kids, wealth and a good health I don't know, I don't care either, this was simply a good choice, the choice of the winners. You are a winner, praise the coffee and stuff...",
@@ -123,8 +123,8 @@ export default {
       description: "Well you'll be missing on one of the greatest pleasures there is, but now you'll be able to spend that time in something of your choice, like sleeping...",
       image: 'images/owl_droplets.png',
       price: {
-        percentage: 5,
-        years: 2
+        percentage: 10,
+        years: 5
       },
       fate: {
         solo: "Only getting some extra time felt like a good idea at the beginning, but as time went on you realized you would prefer to use that extra time in something else, so you soon came back to Luci's Little Trinkets for more. What started as a safe reasonable choice, ended with the purchase of almost every item on the shop, killing you short after the check out, nevermind the age that the counter shows you right now!. How about that you greedy bastard, rejoice!",
@@ -137,8 +137,8 @@ export default {
       description: "Never again worry about getting injured with Toughskin pipe, forget about cutting your finger, getting shot or ran over by a car, looking both ways when crossing a street is for normies. Now you can become your own armour just by smoking the contents of this pipe (one use only), there's only one thing it can't protect you from: burns and fire, you know, what fire gives, fire can take away... Unintended side effects include getting high for a few minutes and in some cases severe nightmares.",
       image: 'images/toughskin_pipe.png',
       price: {
-        percentage: 5,
-        years: 2
+        percentage: 15,
+        years: 5
       },
       fate: {
         solo: "After purchasing Toughskin Pipe, you lived most of your life enjoying the commodities of being indestructible, no longer worried about getting hurt by chance and sometimes forcing the limits of your newly acquired skin. Nothing could ever harm you again, at least nothing from outside... About the nightmares, that was something else entirely, they started short and dull, nothing that could worry you, after all you couldn't be harmed and you knew it even inside your dreams. But as weeks went by, nightmares started popping more and more often, deep and intense, they were showing you layers of horror you never thought possible. Soon you started daydreaming, no matter what you did or where you went nightmares chased. Little by little you started to give in to terror, at first, fleeing from other people, later locking yourself up in small spaces, after a few months in your mind suicide was the only way out. Desperation drove you to jump off your window, to get hit by a train, you even tried shooting yourself in the head but you were found by authorities and locked before you could continue trying. You spent the rest of your life enduring nightmare after nightmare in a padded cell with no escape.",
